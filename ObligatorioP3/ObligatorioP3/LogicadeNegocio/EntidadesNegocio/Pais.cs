cfb2c28a7c62dee36929f69c611608aeaf149219
JohanS2001 <suarezjohan2001@gmail.com>
2022-09-24 00:46:05 +0200

    Atributos/Metodos Pais
    Ajustes clase pais

--- ObligatorioP3/ObligatorioP3/LogicadeNegocio/EntidadesNegocio/Pais.cs
+++ ObligatorioP3/ObligatorioP3/LogicadeNegocio/EntidadesNegocio/Pais.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace LogicadeNegocio.EntidadesNegocio
 {
-    class Pais
+    public class Pais
     {
+        public string _nombre { get; set; }
+        public int _codigo { get; set; }
+        public int _pbi { get; set; }
+        public int _poblacion { get; set; }
+        public string _imagen { get; set; }
+        public Region _region { get; set; }
+
+        
+        public override bool Equals(object obj)
+        {
+            bool noExiste = false;
+            if (obj != null && obj is Pais)
+            {
+                noExiste = this._codigo == ((Pais)obj)._codigo;
+            }
+
+            return noExiste;
+        }
+
+        public bool Equals([AllowNull] Pais other)
+        {
+            return this._codigo == other._codigo;
+        }
     }
 }
--- ObligatorioP3/ObligatorioP3/LogicadeNegocio/EntidadesNegocio/Region.cs
+++ ObligatorioP3/ObligatorioP3/LogicadeNegocio/EntidadesNegocio/Region.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace LogicadeNegocio.EntidadesNegocio
 {
-    class Region
+    public class Region
     {
+        public string _nombreContinente { get; set; }
     }
 }
